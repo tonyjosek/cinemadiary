55630ac3d15aa2e4998cc557e5718bd8f084a820
Tony <tonykurisummoodan>
2023-10-13 16:55:19 +0200

    text color updated

--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -23,7 +23,7 @@ export const MovieDetail = () => {
 
   return (
     <main>
-      <section className="flex flex-wrap justify-around py-5">
+      <section className=" text-gray-700 flex flex-wrap justify-around py-5">
         <div className="max-w-sm flex items-center " >
           <img className="rounded max-h-96" src={image} alt="Movie poster" />
         </div>
@@ -40,7 +40,7 @@ export const MovieDetail = () => {
             <svg className="w-4 h-4 text-yellow-300 mr-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
                 <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
             </svg>
-            <p className="ml-1 text-sm font-bold text-gray-900 dark:text-white">{movie.vote_average}</p>
+            <p className="ml-1 text-sm font-bold text-gray-700 dark:text-white">{movie.vote_average}</p>
             <span className="w-1 h-1 mx-1.5 bg-gray-500 rounded-full dark:bg-gray-400"></span>
             <span className="text-sm font-medium text-gray-900  dark:text-white">{movie.vote_count} reviews</span>
         </div>  
@@ -49,12 +49,12 @@ export const MovieDetail = () => {
           <span>{movie.runtime} min.</span>
         </p>  
         <p className="my-2">
-          <span className="font-bold">Budget: $</span>
-          <span>{movie.budget}</span>
+          <span className="font-bold">Budget: </span>
+          <span>$ {movie.budget}</span>
         </p>
         <p className="my-2">
-          <span className="font-bold">Revenue: $</span>
-          <span>{movie.revenue} </span>
+          <span className="font-bold">Revenue: </span>
+          <span>$ {movie.revenue} </span>
         </p>
         <p className="my-2">
           <span className="font-bold">Release Date: </span>
